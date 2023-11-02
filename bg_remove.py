e80ce95d11cc3b773c62401b2974aa3a9af3965e
EhsanMalik360 <leverify.bi022@gmail.com>
2023-11-02 14:07:37 +0500

    Update bg_remove.py
    title removed.

--- bg_remove.py
+++ bg_remove.py
@@ -6,7 +6,7 @@ import base64
 
 st.set_page_config(layout="wide", page_title="Veritas Image Background Remover")
 
-st.write("## Remove background from your image")
+#st.write("## Remove background from your image")
 st.write(
     "Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar."
 )
